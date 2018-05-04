a775e6a2ca90c3505c58cffce233570633daa728
zhenxjiang <zhenxjiang@foxmail>
2018-05-04 16:37:46 +0800

    新建ch5_2_1项目

---
+++ ch5_2_1/src/main/java/com/wisely/ch5_2_1/Ch521Application.java
@@ -0,0 +1,12 @@
+package com.wisely.ch5_2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch521Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch521Application.class, args);
+	}
+}
